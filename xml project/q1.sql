b53c4123b0d2e6f6189532c762abadd5b5b6952d
SGywzhang <46080332+SGywzhang@users.noreply.github.com>
2019-03-21 11:14:04 +0800

    Update q1.sql

--- xml project/q1.sql
+++ xml project/q1.sql
@@ -1,32 +1,45 @@
 copy(
-SELECT xmlelement(
- name warehouses,
- xmlagg(
-  xmlelement(name warehouse,
-       xmlforest(
-        warehouse.w_id AS id,
-        warehouse.w_name AS name,
-        xmlforest(
-         warehouse.w_street AS street,
-         warehouse.w_city AS city,
-         warehouse.w_country AS country
-        ) AS address,
-        (SELECT xmlagg(
-         xmlelement(name item, 
-          xmlforest(
-           item.i_id AS id,
-           item.i_im_id AS im_id,
-           item.i_name AS name,
-           item.i_price AS price
-          )
-         ) ORDER BY item.i_id
-        )
-        FROM stock, item
-        WHERE stock.w_id = warehouse.w_id
-        AND item.i_id = stock.i_id) AS items
-       )
-      ) ORDER BY warehouse.w_id
- )
-)
-FROM warehouse
-) to '/home/cs4221/Desktop/q1.xml'
+	select xmlroot
+	(
+		xmlelement
+		(
+			name "warehouses",
+			xmlagg
+			(
+				xmlelement
+				(
+					name "warehouse",
+					xmlelement(name "id",warehouse.w_id),
+					xmlelement(name "name",warehouse.w_name),
+					xmlelement
+					(
+						name "address",
+						xmlelement(name "street",warehouse.w_street),
+						xmlelement(name "city",warehouse.w_city),
+						xmlelement(name "country",warehouse.w_country)
+					),
+					xmlelement
+					(
+						name "items",	
+						(
+							select xmlagg
+							(
+								xmlelement
+								(	
+									name "item",
+									xmlelement(name "id",item.i_id),
+									xmlelement(name "im_id",item.i_im_id),
+									xmlelement(name "name",item.i_name),
+									xmlelement(name "price",item.i_price),
+									xmlelement(name "qty",stock.s_qty)
+								)
+								order by item.i_id
+							)  
+							from stock , item where stock.w_id = warehouse.w_id and stock.i_id = item.i_id
+						)
+					)
+				)
+			order by warehouse.W_id) 
+		), version '1.0" encoding = "utf-8' 
+	) from warehouse
+ ) to '/home/cs4221/Desktop/q1.xml'
