e8dc5fc8ca6602acc3ad33dccb1fbd412e3b35ae
SGywzhang <46080332+SGywzhang@users.noreply.github.com>
2019-03-21 10:38:11 +0800

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,38 +1,45 @@
 copy(
-select xmlroot
-(
-	xmlelement
+	select xmlroot
 	(
-		name "warehouses",
-		xmlagg
+		xmlelement
 		(
-			xmlelement
+			name "warehouses",
+			xmlagg
 			(
-				name "warehouse",
-				xmlelement(name "id",warehouse.w_id),
-				xmlelement(name "name",warehouse.w_name),
 				xmlelement
 				(
-					name "address",
-					xmlelement(name "street",warehouse.w_street),
-					xmlelement(name "city",warehouse.w_city),
-					xmlelement(name "country",warehouse.w_country)
-				),
-				xmlelement
-				(
-					name "items",	
-					(select xmlagg(xmlelement
-					(	
-						name "item",
-						xmlelement(name "id",item.i_id),
-						xmlelement(name "im_id",item.i_im_id),
-						xmlelement(name "name",item.i_name),
-						xmlelement(name "price",item.i_price),
-						xmlelement(name "qty",stock.s_qty)
-					)) from stock , item where stock.w_id = warehouse.w_id and stock.i_id = item.i_id)
+					name "warehouse",
+					xmlelement(name "id",warehouse.w_id),
+					xmlelement(name "name",warehouse.w_name),
+					xmlelement
+					(
+						name "address",
+						xmlelement(name "street",warehouse.w_street),
+						xmlelement(name "city",warehouse.w_city),
+						xmlelement(name "country",warehouse.w_country)
+					),
+					xmlelement
+					(
+						name "items",	
+						(
+							select xmlagg
+							(
+								xmlelement
+								(	
+									name "item",
+									xmlelement(name "id",item.i_id),
+									xmlelement(name "im_id",item.i_im_id),
+									xmlelement(name "name",item.i_name),
+									xmlelement(name "price",item.i_price),
+									xmlelement(name "qty",stock.s_qty)
+								)
+								order by item.i_id
+							)  
+							from stock , item where stock.w_id = warehouse.w_id and stock.i_id = item.i_id
+						)
+					)
 				)
-			)
-		) 
-	), version 1.0, standalone yes
-) from warehouse
-) to '/home/cs4221/Desktop/test.xml'
+			order by warehouse.W_id) 
+		), version '1.0" encoding = "utf-8' 
+	) from warehouse
+) to '/home/cs4221/Desktop/q1.xml'
