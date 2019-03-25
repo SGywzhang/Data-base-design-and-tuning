0e90f20e7b523e93ffa679599d3eb09c9c7ac00d
ZHANG YAOWEN <46080332+SGywzhang@users.noreply.github.com>
2019-03-25 18:45:59 +0800

    Update tpch.sql

--- TCP-H project/tpch.sql
+++ TCP-H project/tpch.sql
@@ -96,14 +96,14 @@ CREATE TABLE REGION (
 
 
 ----- Copy the tbl file to /tmp/
-COPY customer FROM '/tmp/customer.tbl' WITH DELIMITER '|';
-COPY lineitem FROM '/tmp/lineitem.tbl' WITH DELIMITER '|';
-COPY nation FROM '/tmp/nation.tbl' WITH DELIMITER '|';
-COPY orders FROM '/tmp/orders.tbl' WITH DELIMITER '|';
-COPY part FROM '/tmp/part.tbl' WITH DELIMITER '|';
-COPY partsupp FROM '/tmp/partsupp.tbl' WITH DELIMITER '|';
-COPY region FROM '/tmp/region.tbl' WITH DELIMITER '|';
-COPY supplier FROM '/tmp/supplier.tbl' WITH DELIMITER '|';
+COPY customer FROM '/home/cs4221/tpch-table/customer.tbl' WITH DELIMITER '|';
+COPY lineitem FROM '/home/cs4221/tpch-table/lineitem.tbl' WITH DELIMITER '|';
+COPY nation FROM '/home/cs4221/tpch-table/nation.tbl' WITH DELIMITER '|';
+COPY orders FROM '/home/cs4221/tpch-table/orders.tbl' WITH DELIMITER '|';
+COPY part FROM '/home/cs4221/tpch-table/part.tbl' WITH DELIMITER '|';
+COPY partsupp FROM '/home/cs4221/tpch-table/partsupp.tbl' WITH DELIMITER '|';
+COPY region FROM '/home/cs4221/tpch-table/region.tbl' WITH DELIMITER '|';
+COPY supplier FROM '/home/cs4221/tpch-table/supplier.tbl' WITH DELIMITER '|';
 
 
 
